898f7c7d54b2215c2f0fbf1b1b5b206bf24a978d
Logan Lang <devloglogan@gmail.com>
2023-01-15 09:59:55 -0600

    added pistol animation code

--- Player.gd
+++ Player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody3D
 
 @onready var camera = $Camera3D
+@onready var anim_player = $AnimationPlayer
 
 const SPEED = 10.0
 const JUMP_VELOCITY = 10.0
@@ -16,6 +17,10 @@ func _unhandled_input(event):
 		rotate_y(-event.relative.x * .005)
 		camera.rotate_x(-event.relative.y * .005)
 		camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2)
+	
+	if Input.is_action_just_pressed("shoot") \
+			and anim_player.current_animation != "shoot":
+		play_shoot_effects()
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -37,4 +42,15 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
+	if anim_player.current_animation == "shoot":
+		pass
+	elif input_dir != Vector2.ZERO and is_on_floor():
+		anim_player.play("move")
+	else:
+		anim_player.play("idle")
+
 	move_and_slide()
+
+func play_shoot_effects():
+	anim_player.stop()
+	anim_player.play("shoot")
