2949a872ba8b897f1b2011cd33015be96275a227
Logan Lang <devloglogan@gmail.com>
2023-01-16 22:20:31 -0600

    main menu setup

--- world.gd
+++ world.gd
@@ -1,5 +1,14 @@
 extends Node
 
+@onready var main_menu = $CanvasLayer/MainMenu
+@onready var address_entry = $CanvasLayer/MainMenu/MarginContainer/VBoxContainer/AddressEntry
+
 func _unhandled_input(event):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
+
+func _on_host_button_pressed():
+	pass # Replace with function body.
+
+func _on_join_button_pressed():
+	pass # Replace with function body.
