a8d4adf36f9a8bf74f22557ee2e2ce47fc122f43
Logan Lang <devloglogan@gmail.com>
2023-01-17 22:49:31 -0600

    added server and client creation

--- world.gd
+++ world.gd
@@ -3,12 +3,30 @@ extends Node
 @onready var main_menu = $CanvasLayer/MainMenu
 @onready var address_entry = $CanvasLayer/MainMenu/MarginContainer/VBoxContainer/AddressEntry
 
+const Player = preload("res://player.tscn")
+const PORT = 9999
+var enet_peer = ENetMultiplayerPeer.new()
+
 func _unhandled_input(event):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
 
 func _on_host_button_pressed():
-	pass # Replace with function body.
+	main_menu.hide()
+	
+	enet_peer.create_server(PORT)
+	multiplayer.multiplayer_peer = enet_peer
+	multiplayer.peer_connected.connect(add_player)
+	
+	add_player(multiplayer.get_unique_id())
 
 func _on_join_button_pressed():
-	pass # Replace with function body.
+	main_menu.hide()
+	
+	enet_peer.create_client("localhost", PORT)
+	multiplayer.multiplayer_peer = enet_peer
+
+func add_player(peer_id):
+	var player = Player.instantiate()
+	player.name = str(peer_id)
+	add_child(player)
